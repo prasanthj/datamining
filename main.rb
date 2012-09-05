63cbf8c728219310064a097e5e0a38b8a7e68d81
prasanthj <j.prasanth.j@gmail.com>
2012-09-05 02:06:13 -0400

    Initial code commit with basic file reading.

---
+++ main.rb
@@ -0,0 +1,18 @@
+data_dir = "data"
+if File.directory?(data_dir) 
+	Dir.foreach(data_dir) { |file| 
+		begin
+			f = File.new(data_dir+"/"+file, "r") unless file == "." || file == ".."
+			while(line = f.gets)
+				puts line
+			end
+		rescue => e
+			puts "ERROR! Opening file."
+			puts "Exception: #{e}"
+			e
+		end
+	}
+else
+	puts "ERROR! Cannot find 'data' directory. Please make sure 'data' directory is available in the current directory"
+end
+	
